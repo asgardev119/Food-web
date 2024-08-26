f3dc554bb0e28cdb5188ad0655bd995fcbbe8f16
asgardev119 <asgar.ies.bpl@gmail.com>
2024-08-26 22:58:16 +0530

    develop

--- src/mockData.js
+++ src/mockData.js
@@ -73,3 +73,4 @@ export const menuApi =
   "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
 
 export const PROXY_CORS = "https://corsproxy.org/?";
+export const generateProxyUrl = (URL) => PROXY_CORS + encodeURIComponent(URL);
